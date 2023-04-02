fea796dc220bdeb8b7db50a7f7b41c7218727d2a
fa1away <su943128817@gmail.com>
2023-04-02 17:56:15 +0800

    v0.3152 irq_test(lab test)

--- Core/Src/stm32h7xx_it.c
+++ Core/Src/stm32h7xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern QSPI_HandleTypeDef hqspi;
 extern UART_HandleTypeDef huart1;
 /* USER CODE BEGIN EV */
 
@@ -266,6 +267,20 @@ void EXTI15_10_IRQHandler(void)
   /* USER CODE BEGIN EXTI15_10_IRQn 1 */
 
   /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles QUADSPI global interrupt.
+  */
+void QUADSPI_IRQHandler(void)
+{
+  /* USER CODE BEGIN QUADSPI_IRQn 0 */
+
+  /* USER CODE END QUADSPI_IRQn 0 */
+  HAL_QSPI_IRQHandler(&hqspi);
+  /* USER CODE BEGIN QUADSPI_IRQn 1 */
+
+  /* USER CODE END QUADSPI_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
